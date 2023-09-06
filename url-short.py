102fa861fea1aa285da574bcbe33e24bfc7b9a4d
PRATHAMESH SURVE <134413659+SPrathamesh2611@users.noreply.github.com>
2023-09-06 19:22:08 +0530

    Update url-short.py
    remove errors

--- url-short.py
+++ url-short.py
@@ -1,6 +1,6 @@
 import pyshorteners  # Import the pyshorteners library for URL shortening
 
-url = input("ENTER URL FOR SHORTENING \n")s#Prompt the user to enter the URL for shortening
+url = input("ENTER URL FOR SHORTENING \n")#Prompt the user to enter the URL for shortening
 
 # Shorten the URL using the pyshorteners library and the TinyURL service
 shortened_url = pyshorteners.Shortener().tinyurl.short(url)
